3696c6fa50f6a7eb3026cf7406731baa29cf29ba
Roman, Badornyi <roman.badornyi@gmail.com>
2023-08-23 17:31:09 +0200

    add pipe comunication protocol and abstraction for pipe server and client.

---
+++ RpcPipes.Transport/PipeProtocol.cs
@@ -0,0 +1,162 @@
+using System.Buffers;
+
+namespace RpcPipes.Transport;
+
+public class PipeProtocol
+{
+    private readonly Stream _stream;
+    private readonly IPipeMessageSerializer _serializer;
+
+    public PipeProtocol(Stream stream, IPipeMessageSerializer serializer)
+    {
+        _stream = stream;
+        _serializer = serializer;
+    }
+
+    public async Task TransferMessage<T>(Guid messageId, int bufferSize, T request, CancellationToken token)
+    {
+        await BeginTranferMessage();
+        await WaitAcknowledge(messageId, token);
+
+        await StartTransferMessage();
+        await WaitAcknowledge(messageId, token);
+
+        async Task BeginTranferMessage()
+        {
+            var messageIdArray = messageId.ToByteArray();
+            var bufferSizeArray = BitConverter.GetBytes(bufferSize);
+            var bufferLength = messageIdArray.Length + bufferSizeArray.Length;
+
+            var buffer = ArrayPool<byte>.Shared.Rent(bufferLength);
+
+            try
+            {
+                Array.Copy(messageIdArray, buffer, messageIdArray.Length);
+                Array.Copy(bufferSizeArray, 0, buffer, messageIdArray.Length, bufferSizeArray.Length);
+                await _stream.WriteAsync(buffer, 0, buffer.Length, token);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+
+        async Task StartTransferMessage()
+        {
+            var chunkBuffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+
+            try
+            {
+                await using var pipeStream = new PipeChunkWriteStream(chunkBuffer, bufferSize, _stream);
+                await _serializer.Serialize(request, pipeStream, token);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(chunkBuffer);
+            }
+        }
+    }
+
+    public async Task<(Guid? messageId, int bufferSize)> BeginReceiveMessage(Action<Guid> onAcceptAction, CancellationToken token)
+    {
+        const int bufferLength = 16 + sizeof(int);
+        var buffer = ArrayPool<byte>.Shared.Rent(bufferLength);
+
+        try
+        {
+            var bufferReadLength = await _stream.ReadAsync(buffer, 0, bufferLength, token);
+            if (bufferReadLength == bufferLength)
+            {
+                var messageIdArray = new byte[16];
+                Array.Copy(buffer, messageIdArray, 16);
+                var messageId = new Guid(messageIdArray);
+                var bufferSize = BitConverter.ToInt32(buffer, 16);
+
+                onAcceptAction?.Invoke(messageId);
+
+                await SendAcknowledge(messageId, true, token);
+                return (messageId, bufferSize);
+            }
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+        return (null, 0);
+    }
+
+    public async Task<T> EndReceiveMessage<T>(Guid messageId, int bufferSize, CancellationToken token)
+    {
+        T message;
+
+        var chunkBuffer = ArrayPool<byte>.Shared.Rent(bufferSize);
+        try
+        {
+            using var pipeStream = new PipeChunkWriteStream(chunkBuffer, bufferSize, _stream);
+            message = await _serializer.Deserialize<T>(pipeStream, token);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(chunkBuffer);
+        }
+
+        await SendAcknowledge(messageId, true, token);
+        return message;
+    }
+
+    public async Task<T> ReceiveMessage<T>(CancellationToken token)
+    {
+        var (messageId, bufferSize) = await BeginReceiveMessage(null, token);
+        if (bufferSize > 0)
+            return await EndReceiveMessage<T>(messageId.Value, bufferSize, token);
+        return default;
+    }
+
+    private async Task WaitAcknowledge(Guid messageId, CancellationToken token)
+    {
+        const int bufferLength = 16 + sizeof(bool);
+        var buffer = ArrayPool<byte>.Shared.Rent(bufferLength);
+
+        var messageIdReceived = Guid.Empty;
+        var ackReceived = false;
+        try
+        {
+            var bufferReadLength = await _stream.ReadAsync(buffer, 0, bufferLength, token);
+            if (bufferReadLength == bufferLength)
+            {
+                var messageIdReceivedArray = new byte[16];
+                Array.Copy(buffer, messageIdReceivedArray, 16);
+                messageIdReceived = new Guid(messageIdReceivedArray);
+                ackReceived = BitConverter.ToBoolean(buffer, 16);
+            }
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+
+        if (messageIdReceived == messageId && ackReceived)
+            return;
+        throw new InvalidOperationException($"Server did not acknowledge receiving of request message {messageId}");
+    }
+
+    private async Task SendAcknowledge(Guid messageId, bool ack, CancellationToken token)
+    {
+        var messageIdArray = messageId.ToByteArray();
+        var ackArray = BitConverter.GetBytes(ack);
+        var bufferLength = messageIdArray.Length + ackArray.Length;
+
+        var buffer = ArrayPool<byte>.Shared.Rent(bufferLength);
+
+        try
+        {
+            Array.Copy(messageIdArray, buffer, messageIdArray.Length);
+            Array.Copy(ackArray, 0, buffer, messageIdArray.Length, ackArray.Length);
+            await _stream.WriteAsync(buffer, 0, bufferLength, token);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(buffer);
+        }
+    }
+}
---
+++ RpcPipes.Transport/PipeTransport.cs
@@ -0,0 +1,191 @@
+using System.IO.Pipes;
+using Microsoft.Extensions.Logging;
+
+namespace RpcPipes.Transport;
+
+public class PipeTransport
+{
+    private readonly ILogger _logger;
+
+    protected readonly PipeOptions Options;
+    protected readonly int Instances;
+    protected readonly int BufferSize;
+
+    public PipeTransport(ILogger logger, int instances, int bufferSize, PipeOptions options)
+    {
+        _logger = logger;
+
+        Instances = instances;
+        BufferSize = bufferSize;
+        Options = options;
+    }
+
+    protected Task StartServerListener(int count, Func<Task> taskAction)
+    {
+        var pipeTasks = new List<Task>();
+        for (var i = 0; i < count; i++)
+        {
+            pipeTasks.Add(Task.Factory.StartNew(taskAction.Invoke, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default).Unwrap());
+        }
+        return Task.WhenAll(pipeTasks);
+    }
+
+    protected async Task<bool> WaitForClientConnection(NamedPipeServerStream server, string pipeName, CancellationToken token)
+    {
+        try
+        {
+            await server.WaitForConnectionAsync(token);
+            return true;
+        }
+        catch (OperationCanceledException)
+        {
+            _logger.LogDebug("connection to {Type} stream pipe {Pipe} closed", "server", pipeName);
+            return false;
+        }
+        catch (IOException)
+        {
+            _logger.LogDebug("connection to {Type} stream pipie {Pipe} got interrupted", "server", pipeName);
+            return false;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "connection to {Type} stream pipe {Pipe} got unhandled error", "server", pipeName);
+            return false;
+        }
+    }
+
+    protected async Task<bool> TryConnectToServer(NamedPipeClientStream client, string pipeName, TimeSpan timeout)
+    {
+        using var connectionCancellation = new CancellationTokenSource();
+        connectionCancellation.CancelAfter(timeout);
+        try
+        {
+            await client.ConnectAsync(Timeout.Infinite, connectionCancellation.Token);
+            return true;
+        }
+        catch (OperationCanceledException)
+        {
+            return false;
+        }
+        catch (IOException)
+        {
+            _logger.LogDebug("connection to {Type} stream pipe {Pipe} got interrupted", "client", pipeName);
+            return false;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "connection to {Type} stream pipe {Pipe} got unhandled error", "client", pipeName);
+            return false;
+        }
+    }
+
+    protected void DisconnectServer(NamedPipeServerStream server, string pipeName)
+    {
+        try
+        {
+            try
+            {
+                if (server.IsConnected)
+                    server.Disconnect();
+            }
+            finally
+            {
+                server.Dispose();
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "unhandled error occured when handling {Type} stream pipe {Pipe} got unhandled error", "server", pipeName);
+        }
+    }
+
+    protected void DisconnectClient(NamedPipeClientStream client, string pipeName)
+    {
+        try
+        {
+            try
+            {
+                client.Close();
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "unhandled error occured when handling {Type} stream pipe {Pipe} got unhandled error", "client", pipeName);
+        }        
+    }
+
+    public async Task RunClientMessageLoop(
+        CancellationToken token, string pipeName, TimeSpan connectionTimeout, Func<NamedPipeClientStream, CancellationToken, Task> action, Action onConnect, Action onDisconnect)
+    {
+        while (!token.IsCancellationRequested) 
+        {
+            var clientPipeStream = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut, Options);
+            try
+            {
+                if (await TryConnectToServer(clientPipeStream, pipeName, connectionTimeout)) {
+                    onConnect.Invoke();
+                    try
+                    {
+                        while (!token.IsCancellationRequested && clientPipeStream.IsConnected)
+                        {
+                            await action.Invoke(clientPipeStream, token);
+                        }
+                    }
+                    finally
+                    {
+                        DisconnectClient(clientPipeStream, pipeName);
+                        onDisconnect.Invoke();
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "unhandled error occured while connecting to the server pipe stream {Pipe}", pipeName);
+            }
+        }
+    }
+
+    public async Task RunServerMessageLoop(
+        CancellationToken token, string pipeName, Func<NamedPipeServerStream, CancellationToken, Task> action, Action onConnect, Action onDisconnect)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            var serverPipeStream = new NamedPipeServerStream(pipeName, PipeDirection.InOut, Instances, PipeTransmissionMode.Byte, Options, BufferSize, BufferSize);
+            try
+            {
+                if (await WaitForClientConnection(serverPipeStream, pipeName, token))
+                {
+                    onConnect.Invoke();
+                    try
+                    {
+                        while (!token.IsCancellationRequested && serverPipeStream.IsConnected)
+                        {
+                            await action.Invoke(serverPipeStream, token);
+                        }
+                    }
+                    finally
+                    {
+                        DisconnectServer(serverPipeStream, pipeName);
+                        onDisconnect.Invoke();
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (Exception e) 
+            {
+                _logger.LogError(e, "unhandled error occured while connecting to the client pipe stream {Pipe}", pipeName);
+            }
+        }
+    }
+}
